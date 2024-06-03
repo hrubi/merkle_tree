3de3b8addfa6c5dc8ae60bcc4bb59d6f6f1b8496
Jan Hruban <hrubi@hrubi.cz>
2024-06-03 14:34:31 +0200

    Clean state

--- lib/merkle_tree_rust.ex
+++
@@ -1,5 +0,0 @@
-defmodule MerkleTreeRust do
-  use Rustler, otp_app: :merkle_tree, crate: "merkle_tree"
-
-  def root(_filename), do: :erlang.nif_error(:nif_not_loaded)
-end
--- perf_test.exs
+++
@@ -1,6 +0,0 @@
-filename = Path.join(__DIR__, "input-perf.txt")
-
-Benchee.run(%{
-  "elixir" => fn -> MerkleTree.root_elixir(filename) end,
-  "rust" => fn -> MerkleTree.root_rust(filename) end
-})
